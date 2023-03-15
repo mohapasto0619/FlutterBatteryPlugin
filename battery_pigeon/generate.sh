1f41d419bafcf19bbc8f6945dd76da2002563161
Meghasli Mohamed <meghasli@TotallySpie.local>
2023-03-15 15:55:31 +0100

    working for ios ans android but need improvement

--- battery_pigeon/generate.sh
+++ battery_pigeon/generate.sh
@@ -1,5 +1,9 @@
 flutter pub run pigeon \
   --input lib/battery_host_api.dart \
   --dart_out ../battery_platform_interface/lib/battery_api.dart \
+  --dart_out ../battery_android/lib/battery_android_api.dart \
+  --dart_out ../battery_ios/lib/battery_ios_api.dart \
   --java_out ../battery_android/android/src/main/java/com/example/battery_android/Battery.java \
-  --java_package "com.example.battery_android"
+  --java_package "com.example.battery_android" \
+  --experimental_swift_out ../battery_ios/ios/Classes/Battery.swift
+  
